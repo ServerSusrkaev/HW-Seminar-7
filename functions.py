8a1628887eef90376e4c8859762686b9635c17a6
Server Susurkaev <susurkaev@yandex.ru>
2023-06-17 07:51:00 +0300

    Task 34, 36 added

---
+++ functions.py
@@ -0,0 +1,10 @@
+def rhythm(str):
+    str = str.split()
+    list_1 = []
+    for word in str:
+        sum_w = 0
+        for i in word:
+            if i in 'аеёиоуыэюя':
+                sum_w += 1
+        list_1.append(sum_w)
+    return len(list_1) == list_1.count(list_1[0])
